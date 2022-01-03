3ef1c90f4cfa09e61dbc8ccc186d50c22cff1e0d
wltnthss <60785586+wltnthss@users.noreply.github.com>
2022-01-04 00:21:31 +0900

    @GetMapping의 url을 받아 @ResponseBody 어노테이션을 활용하여 데이터를 String 또는 Json 형태로 리턴해준다. (기존에 modelandview 를 통해 jsp파일로 setviewname 리턴해주던 방법과 다른방법?)

--- src/main/java/sb/sjs_sb/controller/HelloController.java
+++ src/main/java/sb/sjs_sb/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
@@ -24,5 +25,34 @@ public class HelloController {
         model.addAttribute("name", name);
 
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody   // http 바디부에 데이터를 직접 넣어줌, 문자열 그대로 반환
+    public String hello_string(@RequestParam("name") String name){
+
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody   // 안붙어잇으면 view로 넘기지만 있을 시에 문자 또는 객체를 json 방식으로 넘겨줌
+    public Hello hello_api(@RequestParam("name") String name){
+
+        Hello hello = new Hello();
+        hello.setName(name);
+
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
